7ba54fcb5bd3de5c0e85aa5d12ded1caea5a3868
fatin shahriare sium <sium1206@gmail.com>
2021-12-12 17:11:36 +0000

    alhamdulillah,able to complete the project

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ let db = [
         title: 'Surah Mulk',
         src: 'https://media.blubrry.com/muslim_central_quran/podcasts.qurancentral.com/abdel-kawi-abdul-majid/067.mp3',
         img: 'mulk'
+    },
+    {
+        title: 'Surah Buruj',
+        src: './085.mp3',
+        img: 'buruj'
     }
 ]
 
@@ -16,9 +21,12 @@ let preBtn = document.getElementById('pre-btn')
 let playBtn = document.querySelector('#play-btn')
 let nextBtn = document.getElementById('next-btn')
 let audio = document.getElementById('audio')
+let audioImg = document.getElementById('audio-img')
 let title = document.querySelector('.titlex')
 let progressBar = document.getElementById('progressbar')
 let musicContainer = document.querySelector('.music-container')
+let currentTimex = document.getElementById('music-currentTime')
+let durationTime = document.getElementById('music-durationTime')
 
 let index = 0
 
@@ -42,6 +50,7 @@ function pauseMusic() {
 function nextMusic() {
     index = index + 1 == db.length ? 0 : index + 1
     audio.src = db[index].src
+    audioImg.src = `./img/${db[index].img}.jpg`
     title.innerText = db[index].title
     playMusic()
 }
@@ -49,19 +58,49 @@ function nextMusic() {
 function preMusic() {
     index = index - 1 < 0 ? 0 : index - 1
     audio.src = db[index].src
+    audioImg.src = `./img/${db[index].img}.jpg`
     title.innerText = db[index].title
     playMusic()
 }
 
+function convertSec(timex) {
+    if (timex < 60) {
+        return `00:${timex}`
+    } else {
+        let minutex = Math.floor(timex / 60)
+
+        let sec = (timex % 60)
+        //console.log('timex', timex);
+        //let decemalNumberOfSec = timex / 60
+        //return decemalNumberOfSec.toFixed(2)
+        return `${minutex}:${sec > 9 ? sec : `0${sec}`}`
+
+
+    }
+}
+
+let x = 4876
+
+console.log('ddd', 60 % 60);
+
 audio.addEventListener('timeupdate', (e) => {
 
     let { currentTime, duration } = e.srcElement
-    console.log(currentTime);
-    progressBar.value = currentTime
+    //console.log(currentTime);
+
+    currentTimex.innerText = convertSec(Math.round(currentTime))
+    durationTime.innerText = convertSec(Math.round(duration))
+    return progressBar.value = (currentTime / duration) * 100
 })
 console.log(progressBar.value);
 
+progressBar.addEventListener('change', (e) => {
 
+    let needToGo = audio.duration * e.target.value
+    console.log(needToGo)
+    audio.currentTime = needToGo / 100
+
+})
 
 playBtn.addEventListener('click', () => {
     console.log('clicked')
@@ -82,3 +121,5 @@ preBtn.addEventListener('click', () => {
     preMusic()
 })
 // Reference to an output container, use 'pre' styling for JSON output
+
+audio.addEventListener('ended', nextMusic)
